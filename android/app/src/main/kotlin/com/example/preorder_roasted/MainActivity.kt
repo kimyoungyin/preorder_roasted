537484f49b4d5b284bb216544a0b0fcf99c67fed
kimyoungyin <yin199859@gmail.com>
2023-12-24 23:48:23 +0900

    initial project setup

---
+++ android/app/src/main/kotlin/com/example/preorder_roasted/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.preorder_roasted
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Center(
+            child: Text("This is Home"),
+          ),
+        ),
+      ),
+    );
+  }
+}
